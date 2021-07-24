23699c0d538de2bb212b565884c2edbb57f9f751
Shwetha K <62240096+shwethaGitHub-spec@users.noreply.github.com>
2021-07-24 17:07:41 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -20,32 +20,32 @@ function setup() {
 	var roof_options={
 		isStatic:true			
 	}
-	roof = Bodies.rectangle(500,100,230,20,roof_options);
+	roof = Bodies.rectangle(400,100,230,20,roof_options);
     World.add(world,roof);
 
 	//bobs
     var bob1_options  = {
-		restitution :0.1,
-		stiffness:0.1,
+		restitution :1,
+		friction:0,
 		isStatic:false,
-		
+		density:0.8
 	}
-	bob1 = Bodies.circle(400,300,35,bob1_options);
+	bob1 = Bodies.circle(320,380,20,bob1_options);
 	World.add(world,bob1);
-    bob2 = Bodies.circle(446,300,35,bob1_options);
+    bob2 = Bodies.circle(360,380,20,bob1_options);
 	World.add(world,bob2);
-	bob3 = Bodies.circle(492,300,35,bob1_options);
+	bob3 = Bodies.circle(400,380,20,bob1_options);
 	World.add(world,bob3);
-	bob4 = Bodies.circle(538,300,35,bob1_options);
+	bob4 = Bodies.circle(440,380,20,bob1_options);
 	World.add(world,bob4);
-	bob5 = Bodies.circle(584,300,35,bob1_options);
+	bob5 = Bodies.circle(480,380,20,bob1_options);
 	World.add(world,bob5);
 	console.log (bob1_options);
-	thread1 = new Rope(bob1,roof,-100,0);
-	thread2 = new Rope(bob2,roof,-55,0);
-	thread3 = new Rope(bob3,roof,-10,0)
-	thread4 = new Rope(bob4,roof,30,0);
-	thread5 = new Rope(bob5,roof,75,0);
+	thread1 = new Rope(bob1,roof,-80,0);
+	thread2 = new Rope(bob2,roof,-40,0);
+	thread3 = new Rope(bob3,roof,0,0)
+	thread4 = new Rope(bob4,roof,40,0);
+	thread5 = new Rope(bob5,roof,80,0);
 
     console.log(thread1)
 	Engine.run(engine);
@@ -66,11 +66,11 @@ function draw() {
  thread4.display();
  thread5.display();
   //create ellipse shape for multiple bobs here
-  ellipse(bob1.position.x,bob1.position.y,35);
-  ellipse(bob2.position.x,bob2.position.y,35);
-  ellipse(bob3.position.x,bob3.position.y,35);
-  ellipse(bob4.position.x,bob4.position.y,35);
-  ellipse(bob5.position.x,bob5.position.y,35);
+  ellipse(bob1.position.x,bob1.position.y,40,40);
+  ellipse(bob2.position.x,bob2.position.y,40,40);
+  ellipse(bob3.position.x,bob3.position.y,40,40);
+  ellipse(bob4.position.x,bob4.position.y,40,40);
+  ellipse(bob5.position.x,bob5.position.y,40,40);
 
 //roof.display();
 text("press up arrow 2 times",10,100);
@@ -81,7 +81,7 @@ function keyPressed() {
 	 if (keyCode === UP_ARROW) {
 		  Matter.Body.applyForce(bob1,bob1.position,{x:-50,y:-45}); 
 		}
-		console.log(keyPressed);
+		
 	 }
 
 //
